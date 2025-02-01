50e344db312673fdd239e39ac336ac1344de486b
ren-418 <mrsteevtvw418@gmail.com>
2025-02-02 01:00:49 +0800

    add midddleware

--- src/Routes.ts
+++ src/Routes.ts
@@ -5,25 +5,26 @@ import Vault from "./valut";
 import { upload } from "./middleware/upload";
 
 
+
 const Routes = async (router: express.Router) => {
 
 	//user
 	router.post("/signup", Auth.controllers.signup);
 	router.post("/login", Auth.controllers.login);
 	//check-list
-	router.post("/check-list/create", CheckList.controllers.newCheckList);
-	router.post("/check-list/getDetail", CheckList.controllers.getOneItem);
-	router.post("/check-list/getAllByUser", CheckList.controllers.getWholeItemsByUser);
-	router.post("/check-list/delete", CheckList.controllers.deleteItem);
-	router.post("/check-list/update", CheckList.controllers.updateItem);
-	router.post("/check-list/share", CheckList.controllers.shareItem);
+	router.post("/check-list/create", Auth.controllers.middleware, CheckList.controllers.newCheckList);
+	router.post("/check-list/getDetail", Auth.controllers.middleware, CheckList.controllers.getOneItem);
+	router.post("/check-list/getAllByUser", Auth.controllers.middleware, CheckList.controllers.getWholeItemsByUser);
+	router.post("/check-list/delete", Auth.controllers.middleware, CheckList.controllers.deleteItem);
+	router.post("/check-list/update", Auth.controllers.middleware, CheckList.controllers.updateItem);
+	router.post("/check-list/share", Auth.controllers.middleware, CheckList.controllers.shareItem);
 	//vault
-	router.post("/vault/create", upload.single("file"),Vault.controllers.newVault);
-	router.post("/vault/getDetail", Vault.controllers.getOneItem);
-	router.post("/vault/getAllByUser", Vault.controllers.getWholeItemsByUser);
-	router.post("/vault/delete", Vault.controllers.deleteItem);
-	router.post("/vault/update", upload.single("file"), Vault.controllers.updateItem);
-	router.post("/vault/share", Vault.controllers.shareItem);
+	router.post("/vault/create", Auth.controllers.middleware, upload.single("file"), Vault.controllers.newVault);
+	router.post("/vault/getDetail", Auth.controllers.middleware, Vault.controllers.getOneItem);
+	router.post("/vault/getAllByUser", Auth.controllers.middleware, Vault.controllers.getWholeItemsByUser);
+	router.post("/vault/delete", Auth.controllers.middleware, Vault.controllers.deleteItem);
+	router.post("/vault/update", Auth.controllers.middleware, upload.single("file"), Vault.controllers.updateItem);
+	router.post("/vault/share", Auth.controllers.middleware, Vault.controllers.shareItem);
 
 };
 
